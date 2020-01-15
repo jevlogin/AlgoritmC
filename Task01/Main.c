0db17da9e662a9485346377c23fb0d1f2845dd47
JEVLOGIN <jev.login@users.noreply.github.com>
2020-01-15 21:16:20 +0300

    Проблемы с отправкой на git

--- Task01/Main.c
+++ Task01/Main.c
@@ -44,18 +44,75 @@ double IndexMass(double height, double weight)
 		return weight / PowerMH(height);
 	}
 }
+int MaxTwo(int a, int b)
+{
+	if (a > b)
+	{
+		return a;
+	}
+	else
+	{
+		return b;
+	}
+}
+
+int MaxFour(int a, int b, int c, int d)
+{
+	int max = MaxTwo(a, b);
+	max = MaxTwo(max, c);
+	max = MaxTwo(max, d);
+}
+
+void Swap(int *k, int *f)
+{
+	int temp = *k;
+	*k = *f;
+	*f = temp;
+}
+void SwapNotThree(int* k, int* f)
+{
+	
+	*k = *k + *f;
+	*f = *k - *f;
+	*k = *k - *f;
+}
+
 int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "RUS");
 
 	double humHeight = 0;
 	double humWeight = 0;
-	printf("Введите Рост в сантиметрах:");
+	printf("Введите Рост в сантиметрах или метрах (через запятую!):");
 	scanf("%lf", &humHeight);
-	printf("Введите Вес:");
+	printf("Введите Вес в кг целым числом или с десятыми и сотыми долями (через запятую!):");
 	scanf("%lf", &humWeight);
 
-	printf("Индекс массы человека равен: %.0lf", IndexMass(humHeight, humWeight));
+	printf("Индекс массы человека равен: %.0lf\n\n", IndexMass(humHeight, humWeight));
+	getch();
+	system("cls");
+
+	printf("Задание №2\n");
+
+	int a = 1, b = 3, c = 0, d = 5;
+	printf("Есть числа a = %i, b = %i, c = %i, d = %i\n", a, b, c, d);
+
+	printf("Максимальное из четырех чисел равно: %i \n\n", MaxFour(a, b, c, d));
+	getch();
+	system("cls");
+
+	printf("Задание №3\n");
+
+	int k = 5, f = 6;
+	printf("k = %i, f = %i\n\n", k, f);
+
+	printf("Первый способ с использованием временной третьей переменной\n");
+	Swap(&k, &f);
+	printf("k = %i, f = %i\n\n", k, f);
+	
+	printf("Первый способ без использованием третьей переменной\n");
+	SwapNotThree(&k, &f);
+	printf("k = %i, f = %i\n\n", k, f);
 
 	return 0;
 }
